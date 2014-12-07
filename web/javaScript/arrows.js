e4cb397b3946752c14e84af739c42d86e8589ab6
HelenaSabel <helena.b.sabel@gmail.com>
2014-12-07 16:15:58 -0500

    update

---
+++ web/javaScript/arrows.js
@@ -0,0 +1,4 @@
+/*table.sortable th:not(.sorttable_sorted):not(.sorttable_sorted_reverse):not(.sorttable_nosort):after{
+    content:'<svg height="10" width="10"><polygon points="5,0 10,5 0,5" style="fill:black;stroke-width:1"/></svg><svg xmlns="http://www.w3.org/2000/svg" height="10" width="10"> <polygon points="0,0 5,5 10,0" style="fill:black;stroke-width:1"/></svg>'
+}*/
+
